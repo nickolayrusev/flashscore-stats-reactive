c5b0e9671ddf49c6d14c97f97b80948cb8230435
Nickolay Rusev <nickolay.rusev@gmail.com>
2017-10-04 10:59:59 +0300

    needed config

--- src/main/java/com/jboxers/flashscore/FlashscoreStatsReactiveApplication.java
+++ src/main/java/com/jboxers/flashscore/FlashscoreStatsReactiveApplication.java
@@ -1,12 +1,17 @@
 package com.jboxers.flashscore;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class FlashscoreStatsReactiveApplication {
+
+	@Autowired RedisConnectionFactory factory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlashscoreStatsReactiveApplication.class, args);
